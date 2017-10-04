e3ecd4f571c1fd13544f6cfc5d3f884d45a2ec30
venkat24 <venkatramansrikanth@outlook.com>
2017-10-05 02:45:45 +0530

    Make State implement CommandTaker interface
    - Make State inherit from ICommandTaker
    - Add scaffolding for corresponding functions

--- src/state/include/state/state.h
+++ src/state/include/state/state.h
@@ -12,12 +12,13 @@
 #include "physics/vector.h"
 #include "state/actor/soldier.h"
 #include "state/map/interfaces/i_map.h"
+#include "state/interfaces/i_command_taker.h"
 #include "state/money_manager/money_manager.h"
 #include "state/state_export.h"
 
 namespace state {
 
-class STATE_EXPORT State {
+class STATE_EXPORT State : public ICommandTaker {
 protected:
 	/**
 	 * List of all soldiers in the game
@@ -55,6 +56,92 @@ public:
 	 * @return      Vector of all soldiers in state
 	 */
 	std::vector<Soldier*> GetAllSoldiers();
+
+	/**
+	 * Handles soldier movement
+	 *
+	 * @param[in]  player_id     player to act upon
+	 * @param[in]  soldier_id    soldier to act upon
+	 * @param[in]  position      new position to move the soldier to
+	 *
+	 * @throw      std::exception  if the operation was not possible
+	 */
+	void MoveSoldier(
+		PlayerId player_id,
+		int64_t soldier_id,
+		physics::Vector position
+	);
+
+	/**
+	 * Handles enemy tower attacks
+	 *
+	 * @param[in]  player_id     player to act upon
+	 * @param[in]  soldier_id    soldier who is attacking
+	 * @param[in]  tower_id      opponent tower to attack
+	 *
+	 * @throw      std::exception  if the operation was not possible
+	 */
+	void AttackTower(
+		PlayerId player_id,
+		int64_t soldier_id,
+		int64_t tower_id
+	);
+
+	/**
+	 * Handles enemy soldier attacks
+	 *
+	 * @param[in]  player_id           player to act upon
+	 * @param[in]  soldier_id          soldier who is attacking
+	 * @param[in]  enemy_soldier_id    soldier who is to be attacked
+	 *
+	 * @throw      std::exception  if the operation was not possible
+	 */
+	void AttackSoldier(
+		PlayerId player_id,
+		int64_t soldier_id,
+		int64_t enemy_soldier_id
+	);
+
+	/**
+	 * Handles tower builds
+	 *
+	 * @param[in]  player_id     player to act upon
+	 * @param[in]  tower_id      tower to act upon
+	 * @param[in]  position      position to build the tower at
+	 *
+	 * @throw      std::exception  if the operation was not possible
+	 */
+	void BuildTower(
+		PlayerId player_id,
+		int64_t tower_id,
+		physics::Vector position
+	);
+
+	/**
+	 * Handles tower upgrades
+	 *
+	 * @param[in]  player_id     player to act upon
+	 * @param[in]  tower_id      tower to act upon
+	 *
+	 * @throw      std::exception  if the operation was not possible
+	 */
+	void UpgradeTower(
+		PlayerId player_id,
+		int64_t tower_id
+	);
+
+	/**
+	 * Handles tower self destructs
+	 *
+	 * @param[in]  player_id     player to act upon
+	 * @param[in]  tower_id      opponent tower to attack
+	 *
+	 * @throw      std::exception if the operation was not possible
+	 */
+	void SuicideTower(
+		PlayerId player_id,
+		int64_t tower_id
+	);
 };
 
 }
