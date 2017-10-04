e6ec56f9f1d1af283dea14f83cace7d32671cdf1
venkat24 <venkatramansrikanth@outlook.com>
2017-10-05 02:45:45 +0530

    Make State implement CommandTaker interface
    - Make State inherit from ICommandTaker
    - Add scaffolding for corresponding functions

--- src/state/src/state.cpp
+++ src/state/src/state.cpp
@@ -6,26 +6,74 @@
 #include "state/state.h"
 
 namespace state {
-	State::State() {
-		// Init None
-	}
 
-	State::State(
-		std::vector<Soldier*> p_soldiers,
-		IMap* map,
-		MoneyManager money_manager
-		): map(map),
-		money_manager(money_manager) {
-		for(int i = 0; i < p_soldiers.size(); ++i) {
-			soldiers.emplace_back(std::move(p_soldiers[i]));
-		}
-	}
+State::State() {
+	// Init None
+}
 
-	std::vector<Soldier*> State::GetAllSoldiers() {
-		std::vector<Soldier*> ret_soldiers;
-		for(int i = 0; i < soldiers.size(); ++i) {
-			ret_soldiers.push_back(soldiers[i].get());
-		}
-		return ret_soldiers;
+State::State(
+	std::vector<Soldier*> p_soldiers,
+	IMap* map,
+	MoneyManager money_manager
+	): map(map),
+	money_manager(money_manager) {
+	for(int i = 0; i < p_soldiers.size(); ++i) {
+		soldiers.emplace_back(std::move(p_soldiers[i]));
 	}
+}
+
+std::vector<Soldier*> State::GetAllSoldiers() {
+	std::vector<Soldier*> ret_soldiers;
+	for(int i = 0; i < soldiers.size(); ++i) {
+		ret_soldiers.push_back(soldiers[i].get());
+	}
+	return ret_soldiers;
+}
+
+void State::MoveSoldier(
+	PlayerId player_id,
+	int64_t soldier_id,
+	physics::Vector position
+) {
+	// TODO: Define Function
+}
+
+void State::AttackTower(
+	PlayerId player_id,
+	int64_t soldier_id,
+	int64_t tower_id
+) {
+	// TODO: Define Function
+}
+
+void State::AttackSoldier(
+	PlayerId player_id,
+	int64_t soldier_id,
+	int64_t enemy_soldier_id
+) {
+	// TODO: Define Function
+}
+
+void State::BuildTower(
+	PlayerId player_id,
+	int64_t tower_id,
+	physics::Vector position
+) {
+	// TODO: Define Function
+}
+
+void State::UpgradeTower(
+	PlayerId player_id,
+	int64_t tower_id
+) {
+	// TODO: Define Function
+}
+
+void State::SuicideTower(
+	PlayerId player_id,
+	int64_t tower_id
+) {
+	// TODO: Define Function
+}
+
 }
