fa9b019b2022ab5173fa0106a705ea34444917e5
JHurricane96 <arun.jhurricane@gmail.com>
2017-10-20 19:40:51 +0530

    Relax memory ordering of atomics

--- src/drivers/include/drivers/spin_lock.h
+++ src/drivers/include/drivers/spin_lock.h
@@ -7,9 +7,9 @@ class SpinLock {
 	std::atomic_flag m_flag = ATOMIC_FLAG_INIT;
 
 	public:
-		void Lock()     noexcept {   while(m_flag.test_and_set(std::memory_order_acquire)); }
-		void Unlock()   noexcept {         m_flag.clear(std::memory_order_release);         }
-		bool TryLock() noexcept { return !m_flag.test_and_set(std::memory_order_acquire);  }
+		void Lock()     noexcept {   while(m_flag.test_and_set(std::memory_order_relaxed)); }
+		void Unlock()   noexcept {         m_flag.clear(std::memory_order_relaxed);         }
+		bool TryLock() noexcept { return !m_flag.test_and_set(std::memory_order_relaxed);  }
 };
 
 }
