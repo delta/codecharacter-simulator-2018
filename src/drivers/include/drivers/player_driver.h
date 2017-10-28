3fb7158fd7d8803f072f847d17f5f15123699147
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-10-28 23:15:39 +0530

    Add player driver
    First draft of the player driver that will handle the player updates

---
+++ src/drivers/include/drivers/player_driver.h
@@ -0,0 +1,73 @@
+/**
+ * @file player_driver.h
+ * Declaration for player driver class that drives the game
+ */
+
+#ifndef DRIVERS_PLAYER_DRIVER_H
+#define DRIVERS_PLAYER_DRIVER_H
+
+#include "drivers/drivers_export.h"
+#include "drivers/shared_memory_utils/shared_memory_player.h"
+#include "player_wrapper/player_code_wrapper.h"
+#include "state/player_state.h"
+#include <memory>
+
+namespace drivers {
+
+/**
+ * Drives the game by syncing states
+ */
+class DRIVERS_EXPORT PlayerDriver {
+private:
+	/**
+	 * Number of LLVM IR instructions executed by the player
+	 */
+	static uint64_t instruction_count;
+
+	/**
+	 * An instance of the player code wrapper
+	 */
+	std::unique_ptr<player::PlayerCodeWrapper> player_code_wrapper;
+
+	/**
+	 * Shared memory of the player to write count to
+	 */
+	std::unique_ptr<SharedMemoryPlayer> shm_player;
+
+	/**
+	 * Writes the count to shared memory
+	 */
+	void WriteCountToShm();
+
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param      player_code_wrapper  The player code wrapper
+	 * @param      shm_player           The player's shared memory to write the count to
+	 */
+	PlayerDriver(std::unique_ptr<player::PlayerCodeWrapper> player_code_wrapper,
+	    std::unique_ptr<SharedMemoryPlayer> shm_player);
+
+	/**
+	 * Increment instruction_count by count
+	 *
+	 * @param  count  The count
+	 */
+	static void IncrementCount(uint64_t count);
+
+	/**
+	 * Gets the instruction_count
+	 *
+	 * @return     The count.
+	 */
+	uint64_t GetCount();
+
+	/**
+	 * Runs the player's update and then writes the count to shared memory
+	 */
+	void Update();
+};
+}
+
+#endif
---
+++ src/drivers/src/player_driver.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file player_driver.cpp
+ * Contains definitions for the player driver
+ */
+
+#include "drivers/player_driver.h"
+
+namespace drivers {
+
+uint64_t PlayerDriver::instruction_count = 0;
+
+PlayerDriver::PlayerDriver(
+	std::unique_ptr<player::PlayerCodeWrapper> player_code_wrapper,
+	std::unique_ptr<drivers::SharedMemoryPlayer> shm_player) :
+	player_code_wrapper(std::move(player_code_wrapper)),
+	shm_player(std::move(shm_player)) {}
+
+void PlayerDriver::IncrementCount(uint64_t count) {
+	instruction_count += count;
+}
+
+uint64_t PlayerDriver::GetCount() {
+	return instruction_count;
+}
+
+void PlayerDriver::WriteCountToShm() {
+	shm_player->GetBuffer()->instruction_counter = instruction_count;
+}
+
+void PlayerDriver::Update() {
+	player_code_wrapper->Update();
+	WriteCountToShm();
+}
+
+}
