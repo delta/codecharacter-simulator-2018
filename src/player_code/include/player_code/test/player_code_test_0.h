22fb8b0fc4f26ffe633dcd282ab65a25f77cd00b
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-10-28 23:16:37 +0530

    Add player_code test case
    Adds an instance of IPlayerCode for testing purposes

---
+++ src/player_code/include/player_code/test/player_code_test_0.h
@@ -0,0 +1,23 @@
+#include "player/player_code_export.h"
+#include "player_wrapper/interfaces/i_player_code.h"
+#include "state/player_state.h"
+
+namespace player {
+
+/**
+ * Class where player defines AI code
+ */
+class PLAYER_CODE_EXPORT PlayerCode0 : public IPlayerCode {
+private:
+	state::PlayerState* player_state;
+
+public:
+	PlayerCode0(state::PlayerState* player_state);
+
+	/**
+	 * Player AI update function (main logic of the AI)
+	 */
+	void Update() override;
+};
+
+}
---
+++ src/player_code/test/player_code_test_0.cpp
@@ -0,0 +1,16 @@
+#include "player_code/test/player_code_test_0.h"
+
+namespace player {
+
+PlayerCode0::PlayerCode0(state::PlayerState* player_state)
+    : player_state(player_state) {}
+
+void PlayerCode0::Update() {
+	int a = 0;
+	for(unsigned i = 0; i < 10E5; ++i) {
+		a = a + i;
+		a = a - i;
+	}
+}
+
+}
