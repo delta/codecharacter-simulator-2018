17e6ef7bd6924ea54b1d22c0b3ae7aa33ea1548f
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-10-28 23:08:38 +0530

    Add the player wrapper module
    Adds
    - PlayerCodeWrapper class: A wrapper to the player's code
    - IPlayerCode interface: An interface that the player implements
                             when writing the AI

---
+++ src/player_wrapper/src/player_code_wrapper.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file player_code_wrapper.cpp
+ * Contains definitions for the player code wrapper
+ */
+
+#include "player_wrapper/player_code_wrapper.h"
+
+namespace player {
+
+PlayerCodeWrapper::PlayerCodeWrapper(std::unique_ptr<IPlayerCode> player_code) :
+	player_code(std::move(player_code)) {}
+
+void PlayerCodeWrapper::Update() {
+	player_code->Update();
+}
+}
