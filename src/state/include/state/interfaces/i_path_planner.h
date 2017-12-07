67a948c920275dc467fed221c59adcb3593bc49c
venkat24 <venkatramansrikanth@outlook.com>
2017-12-07 08:25:12 +0530

    Add positional movement logic to PathPlanner
     - Method GetNextPosition takes src, dest, speed and returns position at next turn

--- src/state/include/state/interfaces/i_path_planner.h
+++ src/state/include/state/interfaces/i_path_planner.h
@@ -18,15 +18,36 @@ class STATE_EXPORT IPathPlanner {
 	virtual ~IPathPlanner(){};
 
 	/**
-	 * Gets the subsequent node in a path, given a source and a destination
+	 * Given a source node and a destination node, return the next node
+	 * that must be taken
 	 *
-	 * @param[in]   source      A Vector node, with the current position
-	 * @param[in]   destination A Vector node, with the destination position
+	 * @param[in]     source      The node from which the traversal begins
+	 * @param[in]     destination The node to which the traversal is headed
 	 *
-	 * @return      A Vector element, with the next node to traverse to
+	 * @return        The next node in the traversal from the source
+	 *
+	 * @throw         std::out_of_range When source or destination are out of
+	 *                                  bounds or have an invalid terrain type
 	 */
 	virtual physics::Vector GetNextNode(const physics::Vector &source,
 	                                    const physics::Vector &destination) = 0;
+
+	/**
+	 * Given the source position, destination position, and movement speed,
+	 * return the next position to take. Invokes GetNextNode when necessary
+	 *
+	 * Acts as a convinent wrapper for GetNextNode, performing direct movement
+	 * calculations using the speed value
+	 *
+	 * @param[in]     source       The position where the actor currently is
+	 * @param[in]     destination  The position where the actor needs to be
+	 * @param[in]     speed        The actor's absolute movement speed in units
+	 *
+	 * @return        The next position where the actor will be
+	 */
+	virtual physics::Vector GetNextPosition(const physics::Vector &source,
+	                                        const physics::Vector &destination,
+	                                        int64_t speed) = 0;
 };
 
 #endif
