1fa5421e29ddcff12d0ddfa68db202f206be3145
venkat24 <venkatramansrikanth@outlook.com>
2017-12-08 11:51:41 +0530

    Add tests for PathPlanner positional movement
     - Add a few tests for the GetNextPosition method

--- test/state/path_planner_test.cpp
+++ test/state/path_planner_test.cpp
@@ -21,21 +21,20 @@ class PathPlannerTest : public Test {
 	PathPlannerTest() {
 		// Init Map
 		this->map_size = 5;
-		this->elt_size = 1;
+		this->elt_size = 5;
 		for (int i = 0; i < map_size; ++i) {
 			vector<MapElement> row;
 			// Map Arrangement :
 			// L L L L L
 			// L W W W L
 			// L L L L L
-			// L W W W L
+			// L L L L L
 			// L L L L L
 			for (int j = 0; j < map_size; ++j) {
-				row.push_back(
-				    MapElement(Vector(i * elt_size, j * elt_size),
-				               (i == 1 || i == 3) && (j >= 1 && j <= 3)
-				                   ? TerrainType::WATER
-				                   : TerrainType::LAND));
+				row.push_back(MapElement(Vector(i * elt_size, j * elt_size),
+				                         (i == 1) && (j >= 1 && j <= 3)
+				                             ? TerrainType::WATER
+				                             : TerrainType::LAND));
 			}
 			grid.push_back(row);
 		}
@@ -48,6 +47,7 @@ class PathPlannerTest : public Test {
 };
 
 TEST_F(PathPlannerTest, ValidPathsTests) {
+	// TEST GRID PATHS
 	physics::Vector curr;
 
 	// Standard Path Test
@@ -58,7 +58,7 @@ TEST_F(PathPlannerTest, ValidPathsTests) {
 	// . S>- - |
 	// . W W W |
 	// . . . . D
-	// . W W W .
+	// . . . . .
 	// . . . . .
 	// Path is 0,1 -> 0,2 -> 0,3 -> 0,4 -> 1,4 -> 2,4
 
@@ -82,6 +82,33 @@ TEST_F(PathPlannerTest, ValidPathsTests) {
 	// Should return the same postion
 	curr = path_planner->GetNextNode(source, destination);
 	ASSERT_EQ(curr, source);
+
+	// TEST POSITIONAL PATHS
+	source = physics::Vector(2, 2);
+	destination = physics::Vector(17, 7);
+	int speed = 5;
+
+	// Expect the planner to move vertically, and then horizontally
+	EXPECT_EQ(path_planner->GetNextPosition(source, destination, speed),
+	          Vector(7, 2));
+	EXPECT_EQ(path_planner->GetNextPosition(Vector(7, 2), destination, speed),
+	          Vector(12, 2));
+	EXPECT_EQ(path_planner->GetNextPosition(Vector(12, 2), destination, speed),
+	          Vector(17, 2));
+	EXPECT_EQ(path_planner->GetNextPosition(Vector(17, 2), destination, speed),
+	          destination /*which is Vector(17, 7)*/);
+
+	// Test a case with the destination and source in the same grid space
+	source = physics::Vector(0, 0);
+	destination = physics::Vector(4, 4);
+	speed = 2;
+
+	// Should move there diagonally in two steps
+	EXPECT_EQ(path_planner->GetNextPosition(source, destination, speed),
+	          Vector(2, 2));
+
+	EXPECT_EQ(path_planner->GetNextPosition(Vector(2, 2), destination, speed),
+	          destination);
 }
 
 TEST_F(PathPlannerTest, InvalidPathsTests) {
@@ -105,7 +132,7 @@ TEST_F(PathPlannerTest, InvalidPathsTests) {
 	             std::out_of_range);
 
 	// Invalid Terrain
-	EXPECT_THROW(path_planner->GetNextNode(physics::Vector(3, 3), // Invalid
+	EXPECT_THROW(path_planner->GetNextNode(physics::Vector(1, 3), // Invalid
 	                                       physics::Vector(0, 0)  // Valid
 	                                       ),
 	             std::out_of_range);
@@ -115,7 +142,7 @@ TEST_F(PathPlannerTest, InvalidPathsTests) {
 	                                       ),
 	             std::out_of_range);
 
-	EXPECT_THROW(path_planner->GetNextNode(physics::Vector(3, 3), // Invalid
+	EXPECT_THROW(path_planner->GetNextNode(physics::Vector(1, 3), // Invalid
 	                                       physics::Vector(1, 1)  // Inavlid
 	                                       ),
 	             std::out_of_range);
