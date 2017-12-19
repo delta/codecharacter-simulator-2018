565e4174f9e52fb6b6e7ecb04cf4ae3c209c519b
venkat24 <venkatramansrikanth@outlook.com>
2017-12-19 16:32:02 +0530

    Add basic test for Logger
     - Define LogState to log only Money
     - Add simple test to serialize and deserialize message

--- src/logger/src/logger.cpp
+++ src/logger/src/logger.cpp
@@ -11,6 +11,14 @@ Logger::Logger() : turn_count(0), logs(std::make_unique<proto::Game>()) {}
 
 void Logger::LogState(state::IState *state) {
 	// TODO: Define function
+
+	// Dummy definition for testing
+	proto::GameState *game_state = logs->add_states();
+
+	std::vector<int64_t> money = state->GetMoney();
+	for (auto player_money : money) {
+		game_state->add_money(player_money);
+	}
 }
 
 void Logger::WriteGame(std::ostream &write_stream) {
