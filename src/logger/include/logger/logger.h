9d5fc458b23966103e6d725eb5aa4a32facdd1a1
venkat24 <venkatramansrikanth@outlook.com>
2017-12-21 02:11:42 +0530

    Add LogState logic
     - Add TowerLogEntry struct and tower_logs to Logger - maintains a list
     of towers from the previous turn, so that only the tower's which have changed are logged
     - Remove dummy test from logger test

--- src/logger/include/logger/logger.h
+++ src/logger/include/logger/logger.h
@@ -18,6 +18,15 @@
 namespace logger {
 
 /**
+ * Entry for one tower in the logger
+ */
+struct TowerLogEntry {
+	int64_t id;
+	int64_t hp;
+	int64_t level;
+};
+
+/**
  * Logger class that takes the current game state and logs information
  * Writes log to file system after game is complete
  */
@@ -27,6 +36,13 @@ class LOGGER_EXPORT Logger : public ILogger {
 	 * Number of turns since the start of the game
 	 */
 	int64_t turn_count;
+
+	/**
+	 * A log of each player's towers, indexed by PlayerId
+	 * Maintains the state of the game towers at the previous turn, so that
+	 * only a diff of the changes in towers needs to be logged
+	 */
+	std::vector<std::vector<TowerLogEntry>> tower_logs;
 
 	/**
 	 * Protobuf object holding complete game logs
