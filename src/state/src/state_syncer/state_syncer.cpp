8814464e6f6ec449b208509b5d98d5d17dac0ca0
TanviKumar <tanvikumar777@gmail.com>
2018-01-14 15:07:04 +0530

    Add code to Execute Player Commands
    	- Checks if player tries to make an actor perform more than one action
    	- Calls StateSyncer wrapper functions as player commands

--- src/state/src/state_syncer/state_syncer.cpp
+++ src/state/src/state_syncer/state_syncer.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "state/state_syncer/state_syncer.h"
+#include <algorithm>
 
 namespace state {
 
@@ -12,7 +13,60 @@ StateSyncer::StateSyncer(IState *state) : state(state) {}
 void StateSyncer::ExecutePlayerCommands(
     const std::vector<PlayerState *> &player_states,
     const std::vector<bool> &skip_player_commands_flags) {
-	// TODO: Define function
+	for (int player_id = 0; player_id < player_states.size(); ++player_id) {
+		if (skip_player_commands_flags[player_id] == false) {
+
+			for (auto const &soldiers :
+			     player_states[player_id]->soldiers[player_id]) {
+				// A soldier can only either move, attack tower or another
+				// soldier
+				bool is_attacking_tower = soldiers.tower_target != -1;
+				bool is_attacking_soldier = soldiers.soldier_target != -1;
+				bool is_moving =
+				    soldiers.destination != physics::Vector(-1, -1);
+
+				std::vector<bool> flags{is_attacking_soldier, is_moving,
+				                        is_attacking_tower};
+
+				if (std::count(flags.begin(), flags.end(), true) > 1) {
+					LogErrors(1, "Soldier can perform only one task each turn");
+				} else {
+					if (is_attacking_tower) {
+						AttackTower(static_cast<PlayerId>(player_id),
+						            soldiers.id, soldiers.tower_target);
+					} else if (is_attacking_soldier) {
+						AttackSoldier(static_cast<PlayerId>(player_id),
+						              soldiers.id, soldiers.soldier_target);
+					} else if (is_moving) {
+						MoveSoldier(static_cast<PlayerId>(player_id),
+						            soldiers.id, soldiers.destination);
+					}
+				}
+			}
+
+			for (auto const &tower :
+			     player_states[player_id]->towers[player_id]) {
+				if (tower.upgrade_tower == true && tower.suicide == true) {
+					LogErrors(1, "Tower can perform only one task each turn.");
+				} else if (tower.upgrade_tower == true) {
+					UpgradeTower(static_cast<PlayerId>(player_id), tower.id);
+				} else if (tower.suicide == true) {
+					SuicideTower(static_cast<PlayerId>(player_id), tower.id);
+				}
+			}
+
+			for (int j = 0; j < player_states[player_id]->map.size(); ++j) {
+				for (int k = 0; k < player_states[player_id]->map[j].size();
+				     ++k) {
+					if (player_states[player_id]->map[j][k].build_tower ==
+					    true) {
+						BuildTower(static_cast<PlayerId>(player_id),
+						           physics::Vector(j, k));
+					}
+				}
+			}
+		}
+	}
 }
 
 std::vector<int64_t> StateSyncer::UpdateMainState() {
