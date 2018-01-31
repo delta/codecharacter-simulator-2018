b5dd81227a311114fd74775951742ddb0b93895a
venkat24 <venkatramansrikanth@outlook.com>
2018-01-31 21:18:57 +0000

    Add debug log copying to docker runner.sh

--- docker/runner/runner.sh
+++ docker/runner/runner.sh
@@ -8,5 +8,8 @@ cd /root/simulator/bin
 ./main
 
 # Copy game log to output dir
-mkdir /root/outpur_log
 cp /root/simulator/bin/game.log /root/output_log
+
+# Copy debug logs to output dir
+cp /root/simulator/bin/player_1.dlog /root/output_log
+cp /root/simulator/bin/player_2.dlog /root/output_log
